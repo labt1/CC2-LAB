a2585d393bb8dd60f2ef439c3503d297443a2f3f
Yojancs <38123887+Yojancs@users.noreply.github.com>
2019-04-17 09:31:48 -0500

    Update 5Euler.cpp

--- 5Euler.cpp
+++ 5Euler.cpp
@@ -1,21 +1,21 @@
 #include <iostream>
 using namespace std;
 
-int mcm(int val1 , int val2){
-    int mini=min(val1,val2),mcd;
-    for (int i = 1;i<=mini;i++){
-        if (val1%i==0 & val2%i==0){
+int mcm(int num1 , int num2){
+    int m=min(num1,num2),mcd=0;
+    for (int i = 1;i<=m;i++){
+        if (num1%i==0 & num2%i==0){
             mcd=i;
         }
     }
-    return ((val1*val2)/mcd);
+    return ((num1*num2)/mcd);
 }
 
 int main(){
     int num=1;
     for(int i=1;i<20;i++){
         num=mcm(num,i);
-        cout <<num<<endl;
     }
+  cout <<num<<endl;
     return 0;
 }
